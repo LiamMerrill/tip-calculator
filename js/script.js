c068ff8b1f283a9b43fbf5d0fcbf18053fb4696b
Liam <LiamMerrill@gmail.com>
2015-03-16 20:46:51 -0400

    tip-calculator

--- js/script.js
+++ js/script.js
@@ -1,1 +1,18 @@
-//add script here
+function tipCalculate(){
+	var totalBill = document.getElementById('bill').value;
+	console.log(totalBill);
+
+var percBtns = document.getElementsByClassName('tip-btn');
+console.log(percBtns);
+for(var i=0; i<percBtns.length; i++){
+	if (percBtns[i].checked){
+		var tipPerc = percBtns[i].value;
+	}
+}
+var tipTotal = totalBill * tipPerc;
+console.log(tipTotal);
+var tipBox = document.getElementById('tipamount');
+tipBox.innerHTML = '$' + tipTotal;
+var Final = document.getElementById('finalbill');
+tipBox.innerHTML = '$' + totalBill;
+}//add script here

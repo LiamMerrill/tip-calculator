f71379b118fb21ec100b250282faef7062bb4ab6
Liam <LiamMerrill@gmail.com>
2015-03-23 15:05:16 -0400

    First commit

--- js/script.js
+++ js/script.js
@@ -2,17 +2,30 @@ function tipCalculate(){
 	var totalBill = document.getElementById('bill').value;
 	console.log(totalBill);
 
-var percBtns = document.getElementsByClassName('tip-btn');
-console.log(percBtns);
-for(var i=0; i<percBtns.length; i++){
-	if (percBtns[i].checked){
-		var tipPerc = percBtns[i].value;
+	var percBtns = document.getElementsByClassName('tip-btn');
+	console.log(percBtns);
+	for(var i=0; i<percBtns.length; i++){
+		if (percBtns[i].checked){
+			var tipPerc = percBtns[i].value;
+		}
+	}
+	var tipTotal = totalBill * tipPerc;
+	console.log(tipTotal);
+	var tipBox = document.getElementById('tipamount');
+	tipBox.innerHTML = '$' + tipTotal;
+}//add script here
+
+function resetBill(){
+	var totalSpend = document.getElementById('bill');
+	totalSpend.value = "";
+
+	var tipBoxx = document.getElementById('tipamount');
+	tipBoxx.innerHTML = "";
+
+	var percBtns = document.getElementsByClassName('tip-btn');
+	console.log(percBtns);
+	for(var i=0; i<percBtns.length; i++){
+		percBtns[i].checked = false
+		
 	}
 }
-var tipTotal = totalBill * tipPerc;
-console.log(tipTotal);
-var tipBox = document.getElementById('tipamount');
-tipBox.innerHTML = '$' + tipTotal;
-var Final = document.getElementById('finalbill');
-tipBox.innerHTML = '$' + totalBill;
-}//add script here

9c99a55049918477a3fb08a68dc906f2f9e73a10
Liam <LiamMerrill@gmail.com>
2015-03-23 19:27:15 -0400

    Tip Calculator NaN

--- js/script.js
+++ js/script.js
@@ -1,31 +1,37 @@
-function tipCalculate(){
-	var totalBill = document.getElementById('bill').value;
-	console.log(totalBill);
+	function tipCalculate(){
+		var totalBill = document.getElementById('bill').value;
+		console.log(totalBill);
 
-	var percBtns = document.getElementsByClassName('tip-btn');
-	console.log(percBtns);
-	for(var i=0; i<percBtns.length; i++){
-		if (percBtns[i].checked){
-			var tipPerc = percBtns[i].value;
+		var percBtns = document.getElementsByClassName('tip-btn');
+		
+		for(var i=0; i<percBtns.length; i++){
+			if (percBtns[i].checked){
+				var tipPerc = percBtns[i].value;
+			}
+		}
+
+		if(!isNaN(totalBill)){
+			var tipTotal = Math.floor(totalBill * tipPerc * 100) / 100;
+			var tipBox = document.getElementById('tipamount'); 
+			tipBox.innerHTML = '$'+tipTotal;
+		} else {
+			alert('Please enter numbers only');
+			resetBill();
+		}
+		}
+		
+
+	function resetBill(){
+		var totalSpend = document.getElementById('bill');
+		totalSpend.value = "";
+
+		var tipBoxx = document.getElementById('tipamount');
+		tipBoxx.innerHTML = "";
+
+		var percBtns = document.getElementsByClassName('tip-btn');
+		console.log(percBtns);
+		for(var i=0; i<percBtns.length; i++){
+			percBtns[i].checked = false
+			
 		}
 	}
-	var tipTotal = totalBill * tipPerc;
-	console.log(tipTotal);
-	var tipBox = document.getElementById('tipamount');
-	tipBox.innerHTML = '$' + tipTotal;
-}//add script here
-
-function resetBill(){
-	var totalSpend = document.getElementById('bill');
-	totalSpend.value = "";
-
-	var tipBoxx = document.getElementById('tipamount');
-	tipBoxx.innerHTML = "";
-
-	var percBtns = document.getElementsByClassName('tip-btn');
-	console.log(percBtns);
-	for(var i=0; i<percBtns.length; i++){
-		percBtns[i].checked = false
-		
-	}
-}
